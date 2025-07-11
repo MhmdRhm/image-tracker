c6a41e122aba849f1a9e1dc9035ff8b1eca47b2a
MhmdRhm <mhmdrhm.it@gmail.com>
2025-07-11 22:58:01 +0330

    Added better OS detection

--- controllers/images.js
+++ controllers/images.js
@@ -1,24 +1,26 @@
 const express = require('express');
 const path = require('path');
 const fs = require("fs");
+const UAParser = require("ua-parser-js");
 
 const imageAccessLogs = [];
 
 const getImage = (req,res) => {
+    const parser = new UAParser();
     const imageName = req.params.imageName;
     const clientIP = req.ip || req.connection.remoteAddress || req.headers['x-forwarded-for'];
     const userAgent = req.get('User-Agent');
+    const os = parser.setUA(userAgent).getResult();
     const referrer = req.get('Referrer');
     const utcDate = new Date();
     const timestamp = new Date(utcDate.getTime() + (3.5 * 60 * 60 * 1000));
 
-
     imageAccessLogs.push({
         imageName,
         clientIP,
-        userAgent,
         referrer,
-        timestamp
+        timestamp,
+        os
     });
     
 
